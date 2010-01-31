8e5afa11de4fa3a8e6697dd9876c42b9c41ab502
Patrick Murray-John <pgosetti@umw.edu>
2010-01-31 17:27:02 -0500

    cleaned config

---
+++ config.php
@@ -0,0 +1,97 @@
+<?php
+date_default_timezone_set('America/New_York');
+session_set_cookie_params(60 * 60 * 24 * 14);
+
+$domain = $_SERVER['HTTP_HOST'];
+
+if ($domain == 'localhost') {
+
+
+		$config = array(
+		  /* db */
+		  'db_host' => 'localhost', /* optional, default is localhost */
+		  'db_name' => '***',
+		  'db_user' => '***',
+		  'db_pwd' => '****',
+
+
+		  /* store name */
+		  'store_name' => '',
+		
+		
+				
+		  /* endpoint */
+		  'endpoint_features' => array(
+			'select', 'construct', 'ask', 'describe', 
+			'load', 'insert', 'delete', 
+			'dump' /* dump is a special command for streaming SPOG export */
+		  ),
+		  'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+		  'endpoint_read_key' => '***', /* optional */
+		  'endpoint_write_key' => '****', /* optional */		
+		);
+	
+	} else {
+
+		$config = array(
+		  /* db */
+		  'db_host' => 'localhost', /* optional, default is localhost */
+		  'db_pwd' => '***',
+		 'db_name' => '***',   
+		  'db_user' => '***', 
+
+		  /* store name */
+		  'store_name' => '',
+		
+
+			/* endpoint */
+			'endpoint_features' => array(
+			'select', 'construct', 'ask', 'describe', 
+			'load', 'insert', 'delete', 
+			'dump' /* dump is a special command for streaming SPOG export */
+			),
+			'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+			'endpoint_read_key' => '***', /* optional */
+			'endpoint_write_key' => '****', /* optional */
+		
+		);
+				
+		
+}
+	
+	
+$graphConfig = array(
+  'ns' => array(
+	'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+	'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
+	'dbpedia' => 'http://dbpedia.org/ontology/',
+	'foaf' => 'http://xmlns.com/foaf/0.1/',
+	'xsd' => 'http://www.w3.org/2001/XMLSchema#',
+	'dcterms' => 'http://purl.org/dc/terms/',
+	'dc' => 'http://purl.org/dc/elements/1.1/' , 
+	'sioc' => 'http://rdfs.org/sioc/ns#' ,
+	'xsd' => 'http://www.w3.org/2001/XMLSchema#',
+	'r' => 'http://code.rubrick-jetpack.org/vocab/',
+	'r_d' => 'http://data.rubrick-jetpack.org/',
+	'r_i' => 'http://code.rubrick-jetpack.org/vocab/individuals/'
+  )
+);	
+
+$config['ns'] = $graphConfig['ns'];
+
+$prefixes = "PREFIX r: <http://code.rubrick-jetpack.org/vocab/>
+PREFIX r_i: <http://code.rubrick-jetpack.org/vocab/individuals/>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX sioc: <http://rdfs.org/sioc/ns#> 
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>" ;
+
+define('BASE_DIR', dirname(__FILE__) . '/');
+define('ARC_DIR', BASE_DIR . '/libs/arc/');
+define('CLASSES_DIR', BASE_DIR . '/classes/');
+define('PREFIXES', $prefixes);
+
+
+	
+?>
